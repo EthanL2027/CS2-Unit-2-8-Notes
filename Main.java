c107f493548b14f85d1e2f2c55f2f5e51084eae3
Ethan <Ethan.lynn2027@gbwl.org>
2025-11-14 13:54:20 +0000

    2.7 Notes

--- Main.java
+++ Main.java
@@ -2,5 +2,29 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
-   }
-}
+
+      // While loops repeat instructions in the loop body
+      // as long as a certain CONDITION is TRUE
+
+      // COUNTER-CONTROLLED while loop
+      // STEP #1: Initialize control variable
+      int count =1;
+      // STEP #2: Test a conditiom
+      while ( count <=10  ) {
+         // Loop body executes while count <= 5 is true
+         System.out.println( count );
+
+         // STEP #3: Change the variable
+         // (do this after the statements you want to repeat)
+         count++;
+
+
+      }
+
+
+
+
+
+   } // END MAIN METHOD
+} // END CLASS
+
